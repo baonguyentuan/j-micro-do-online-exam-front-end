e6e28b8b5bda23d6b5c39d110f621b25bf411e6a
baonguyentuan <nguyentuanbao.1994@gmail.com>
2023-08-16 23:04:16 +0700

    merge payment

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -22,7 +22,6 @@ const SideBar: React.FC = () => {
   return (
     <Sider theme='dark' collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
       <Menu selectedKeys={[optionSidebarAdmin]} theme="dark" mode="inline" >
-        <Menu.Item key="report" icon={<AreaChartOutlined />}>Analytics</Menu.Item>
         <Menu.Item key={Constants.optionMenuAdmin.USER} icon={<TeamOutlined />}>
           <NavLink to={AppRoutes.private.admin.user}>User Management</NavLink></Menu.Item>
         <Menu.Item key={Constants.optionMenuAdmin.CATEGORY} icon={<AppstoreOutlined />}>
@@ -31,7 +30,6 @@ const SideBar: React.FC = () => {
           <Menu.Item key={Constants.optionMenuAdmin.AUTH.ROLE}><NavLink to={AppRoutes.private.admin.role}>Role</NavLink></Menu.Item>
           <Menu.Item key={Constants.optionMenuAdmin.AUTH.ENDPOINT}><NavLink to={AppRoutes.private.admin.endpoint}>Endpoint</NavLink></Menu.Item>
         </Menu.SubMenu>
-        <Menu.Item key={Constants.optionMenuAdmin.ARTICLES} icon={<FileOutlined />}><NavLink to={AppRoutes.private.admin.article}>Articles</NavLink></Menu.Item>
         <Menu.Item key={Constants.optionMenuAdmin.EXAM} icon={<FileOutlined />}><NavLink to={AppRoutes.private.admin.exam}>Exam</NavLink></Menu.Item>
         <Menu.Item key={Constants.optionMenuAdmin.CONTEST} icon={<BookOutlined />}><NavLink to={AppRoutes.private.admin.contest}>Contest</NavLink></Menu.Item>
         <Menu.Item key="logout" icon={<LogoutOutlined />}>
